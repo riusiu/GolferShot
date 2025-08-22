e5224768a347d4a5ea322737fbd2f3cabe3f1ded
riusiu <ryusho.n.0216@gmail.com>
2025-08-22 15:58:54 +0900

    マテリアル、タイトル、エントリー画面

---
+++ Assets/Scripts/Scenen/CreditChange Scene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public partial class CreditchangeScene : MonoBehaviour
+{
+    public void OnClickCreditButton()
+    {
+        SceneManager.LoadScene("CREDITScene");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
