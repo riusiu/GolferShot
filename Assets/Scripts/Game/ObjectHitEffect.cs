c8996c7a489513a69b6b0f9b8a41c4f539e71d4f
Nekoma46Buddies <ryota9123@icloud.com>
2025-09-19 16:37:08 +0900

    Efffect用スクリプト

---
+++ Assets/Scripts/Game/ObjectHitEffect.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHitEffect : MonoBehaviour
+{
+    [SerializeField]private ParticleSystem particle;
+     private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            //// パーティクルシステムのインスタンスを生成する。
+            ParticleSystem newParticle = Instantiate(particle);
+            // パーティクルの発生場所をこのスクリプトをアタッチしているGameObjectの場所にする。
+            newParticle.transform.position = this.transform.position;
+            // パーティクルを発生させる。
+            newParticle.Play();
+           
+        }
+
+        if (other.gameObject.CompareTag("Taiyaki"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("FrenchBread"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Banana"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("CarolinaReaper"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Apple"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("WhiteRadish"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Cake"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Iron"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Range"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Meat"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Fan"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Bullfight"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Tuna"))
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+            newParticle.transform.position = this.transform.position;
+            newParticle.Play();
+        }
+    }
+}
